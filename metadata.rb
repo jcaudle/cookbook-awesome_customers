f9ef6b6a9abc6149e5b7c5fa3467abe4b70ab080
Joseph Caudle <joseph@josephcaudle.com>
2016-01-08 01:43:33 -0500

    Run chef generate cookbook and update metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'awesome_customers'
+maintainer 'Joseph Caudle'
+maintainer_email 'joseph@josephcaudle.com'
+license 'all_rights'
+description 'Installs/Configures awesome_customers'
+long_description 'Installs/Configures awesome_customers'
+version '0.1.0'
