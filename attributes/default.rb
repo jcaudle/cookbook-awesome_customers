7834ed08412214c5f7779c04c106fefe5992bada
Joseph Caudle <joseph@josephcaudle.com>
2016-01-08 14:15:31 -0500

    Save where the secret will be on the node

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,5 @@ default['awesome_customers']['group'] = 'web_admin'
 default['awesome_customers']['document_root'] = '/var/www/customers/public_html'
 
 default['firewall']['allow_ssh'] = true
+
+default['awesome_customers']['passwords']['secret_path'] = '/etc/chef/encrypted_data_bag_secret'
