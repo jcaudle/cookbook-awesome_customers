a21e3868678b397e71a2fd183ade61464e253b74
Joseph Caudle <joseph@josephcaudle.com>
2016-01-12 20:56:35 -0500

    Bump minor version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joseph@josephcaudle.com'
 license 'all_rights'
 description 'Installs/Configures awesome_customers'
 long_description 'Installs/Configures awesome_customers'
-version '0.2.2'
+version '0.3.0'
 
 depends 'apt', '~> 2.6.1'
 depends 'httpd', '~> 0.2.14'
